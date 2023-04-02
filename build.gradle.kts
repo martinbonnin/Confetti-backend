c8237384d4c145a4e4efa9b3c4277d691a9a3373
Martin Bonnin <martin@mbonnin.net>
2023-04-03 00:09:17 +0200

    move stuff to the end

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,54 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.8.20"
 	id("org.jetbrains.kotlin.plugin.serialization") version "1.8.20"
 }
 
-repositories {
-	mavenCentral()
-}
-
 dependencies {
 	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+repositories {
+	mavenCentral()
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
