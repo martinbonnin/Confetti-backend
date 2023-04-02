5ee9089532f36abe5c12c9d9e99817a2982d2017
Martin Bonnin <martin@mbonnin.net>
2023-04-02 18:53:18 +0200

    add imports

--- src/main/kotlin/confetti/backend/application.kt
+++ src/main/kotlin/confetti/backend/application.kt
@@ -9,11 +9,9 @@ import graphql.schema.GraphQLType
 import kotlinx.datetime.LocalDateTime
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Component
+import org.springframework.context.support.beans
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
-
 
 
 
