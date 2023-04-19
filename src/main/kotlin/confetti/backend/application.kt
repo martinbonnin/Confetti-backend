42d68d0ee5fd03e375909a0448f7f557cfa536fd
Martin Bonnin <martin@mbonnin.net>
2023-04-19 19:05:56 +0200

    add imports

--- src/main/kotlin/confetti/backend/application.kt
+++ src/main/kotlin/confetti/backend/application.kt
@@ -2,18 +2,21 @@ package confetti.backend
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDescription
 import com.expediagroup.graphql.generator.hooks.SchemaGeneratorHooks
+import com.expediagroup.graphql.server.ktor.GraphQL
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.ktor.graphiQLRoute
 import com.expediagroup.graphql.server.operations.Query
 import graphql.language.StringValue
 import graphql.schema.Coercing
 import graphql.schema.GraphQLScalarType
 import graphql.schema.GraphQLType
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.routing.*
 import kotlinx.datetime.LocalDateTime
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.context.support.beans
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
-
 
 
 
